5f0625ffc1f821e99eb5763c669c948d02f98f34
Denis Mikhailov <dmikhailov@smass.tech>
2020-12-13 21:54:22 +0400

    simple 'string' parser was implemented

---
+++ src/json/ast/common.hpp
@@ -0,0 +1,65 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <boost/spirit/home/x3.hpp>
+
+namespace json_client {
+namespace x3 = boost::spirit::x3;
+
+struct unfinished_guard : x3::parser<unfinished_guard> {
+    using attribute_type = char;
+
+    template <typename Iterator, typename Context, typename RContext, typename Attribute>
+    bool parse(Iterator& first, Iterator const& last, Context const& context,
+        RContext const& rcontext, Attribute& attr) const
+    {
+        if (first == last) {
+            boost::throw_exception(
+                x3::expectation_failure<Iterator>(
+                    first, "unfinished string"));
+        }
+        if (*first == '\n' || *first == '\r') {
+            boost::throw_exception(
+                x3::expectation_failure<Iterator>(
+                    first, "unfinished string"));
+        }
+        return false;
+    }
+};
+
+struct esc_guard : x3::parser<esc_guard> {
+    using attribute_type = char;
+
+    template <typename Iterator, typename Context, typename RContext, typename Attribute>
+    bool parse(Iterator& first, Iterator const& last, Context const& context,
+        RContext const& rcontext, Attribute& attr) const
+    {
+        if (first != last && *first == '\\') {
+            boost::throw_exception(
+                x3::expectation_failure<Iterator>(
+                    first, "invalid escape sequence"));
+        }
+        return false;
+    }
+};
+} // end of namespace json_client
+
+template <>
+struct json_client::x3::get_info<json_client::unfinished_guard> {
+    typedef std::string result_type;
+    std::string operator()(json_client::unfinished_guard const&) const
+    {
+        return "unfinished_guard";
+    }
+};
+
+template <>
+struct json_client::x3::get_info<json_client::esc_guard> {
+    typedef std::string result_type;
+    std::string operator()(json_client::esc_guard const&) const
+    {
+        return "esc_guard";
+    }
+};
+
+#endif // COMMON_HPP
--- src/json/ast/value_def.hpp
+++ src/json/ast/value_def.hpp
@@ -2,37 +2,54 @@
 #define INC_AST_VALUE_DEF_HPP
 
 #include "ast.hpp"
+#include "common.hpp"
 #include "value.hpp"
 #include <boost/spirit/home/x3.hpp>
+#include <mutex>
 
 namespace json_client {
 namespace parser {
+    using x3::char_;
     using x3::double_;
     using x3::int_;
+    using x3::lexeme;
+
+    auto const unfinished_guard_ = unfinished_guard {};
+    auto const esc_guard_ = esc_guard {};
+    x3::symbols<> null_kw;
 
     void add_keywords()
     {
-        // TODO: implement this
+        static std::once_flag once;
+        std::call_once(once, [&]() {
+            null_kw.add("null");
+
+            // TODO: escape-символы
+        });
     }
 
     struct value_class;
     struct array_class;
+    struct quoted_class;
 
     typedef x3::rule<value_class, ast::value> value_type;
     typedef x3::rule<array_class, ast::array> array_type;
+    typedef x3::rule<quoted_class, std::string> quoted_type;
 
     value_type const value("value");
     array_type const array("array");
+    quoted_type const quoted("quoted");
 
-    auto const value_def = int_ | double_ | array; // TODO: парсинг строки
+    auto const value_def = int_ | double_ | array | quoted; // TODO: парсинг объекта
     auto const array_def = x3::lit('[')
         > -(value % ',')
         > x3::lit(']');
+    auto const quoted_def = lexeme['"' >> *(unfinished_guard_ | (esc_guard_) | (char_ - '"')) >> '"'];
 
     // TODO: вспомнить чем отличается ">" от ">>"
     // TODO: вспомнить про x3::lit
 
-    BOOST_SPIRIT_DEFINE(value, array);
+    BOOST_SPIRIT_DEFINE(value, array, quoted);
 
 } // end of namespace parser
 } // end of namespace json_client
